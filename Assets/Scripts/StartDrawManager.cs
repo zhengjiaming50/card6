d3be6b75a3963c53c1d17aae35c0ad70112e5022
zhengjiaming50 <zhengzhetian50@gmail.com>
2024-11-17 20:03:09 +0800

    CanNotVideo

--- Assets/Scripts/StartDrawManager.cs
+++ Assets/Scripts/StartDrawManager.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class StartDrawManager : MonoBehaviour
 {
     [SerializeField] private Button startDrawButton;
+    [SerializeField] private GameObject videoPanel;
+    [SerializeField] private VideoPlayer videoPlayer;
 
     void Start()
     {
@@ -15,11 +18,39 @@ public class StartDrawManager : MonoBehaviour
         {
             Debug.LogError("Start Draw Button is not assigned in the inspector.");
         }
+
+        if (videoPlayer != null)
+        {
+            videoPlayer.loopPointReached += OnVideoFinished;
+        }
+        else
+        {
+            Debug.LogError("Video Player is not assigned in the inspector.");
+        }
     }
 
     private void OnStartDrawClicked()
     {
         Debug.Log("Start Draw button clicked!");
-        // TODO: Implement draw logic here
+        PlayDrawAnimation();
+    }
+
+    private void PlayDrawAnimation()
+    {
+        if (videoPanel != null && videoPlayer != null)
+        {
+            videoPanel.SetActive(true);
+            videoPlayer.Play();
+        }
+        else
+        {
+            Debug.LogError("Video Panel or Video Player is not assigned.");
+        }
+    }
+
+    private void OnVideoFinished(VideoPlayer vp)
+    {
+        videoPanel.SetActive(false);
+        Debug.Log("Draw animation completed!");
     }
 }
